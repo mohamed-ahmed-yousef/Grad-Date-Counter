9f021028a2af1483d0ace7d2630349a99f3cd8c7
mohamedahmed-cloud <81932663+mohamedahmed-cloud@users.noreply.github.com>
2024-03-27 12:08:43 +0200

    update Timer & home

---
+++ app/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <section className=" bg-gradient-to-r from-orange-400 via-orange-500 to-orange-400 flex items-center justify-between gap-x-3 px-10 min-h-[50px]">
+      <p className="text-xs md:text-sm">
+        العلم يرفع بيتا لا عماد له، والجهل يهدم بيت العز والشرف
+      </p>
+      <p className="text-sm md:text-base">
+        كليه النهدسة - قسم الحاسبات والمعلومات - 2024
+      </p>
+      <p className="text-xs md:text-sm">
+        لا تحسبن العلم ينفع وحده ما لم يتوج ربه بخلاق
+      </p>
+    </section>
+  );
+}
---
+++ app/components/Timer/Countdown.tsx
@@ -0,0 +1,78 @@
+"use client";
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
+
+const COUNTDOWN_FROM = "7/31/2024";
+
+const SECOND = 1000;
+const MINUTE = SECOND * 60;
+const HOUR = MINUTE * 60;
+const DAY = HOUR * 24;
+
+export default function Countdown() {
+  const [remaining, setRemaining] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    setInterval(handleCountdown, 1000);
+  }, []);
+
+  const handleCountdown = () => {
+    const end = new Date(COUNTDOWN_FROM);
+
+    const now = new Date();
+
+    const distance = +end - +now;
+
+    const days = Math.floor(distance / DAY);
+    const hours = Math.floor((distance % DAY) / HOUR);
+    const minutes = Math.floor((distance % HOUR) / MINUTE);
+    const seconds = Math.floor((distance % MINUTE) / SECOND);
+
+    setRemaining({
+      days,
+      hours,
+      minutes,
+      seconds,
+    });
+  };
+
+  return (
+    <div className="p-4 bg-gradient-to-b from-[#000] via-[#011] to-black min-h-[200px]">
+      <div className="w-full max-w-5xl mx-auto flex items-center ">
+        <CountdownItem num={remaining.days} text="يوما" />
+        <CountdownItem num={remaining.hours} text="ساعة" />
+        <CountdownItem num={remaining.minutes} text="دقيقة" />
+        <CountdownItem num={remaining.seconds} text="ثانية" />
+      </div>
+    </div>
+  );
+}
+
+function CountdownItem({ num, text }: { num: number; text: string }) {
+  return (
+    <div className="font-mono w-1/4 h-24 md:h-36 flex flex-col gap-1 md:gap-2 items-center justify-center">
+      <span className="text-xs md:text-sm lg:text-base font-light text-slate-400">
+        {text}
+      </span>
+      <div className="w-full text-center relative overflow-hidden font-[400]">
+        <AnimatePresence mode="popLayout">
+          <motion.span
+            key={num}
+            initial={{ y: "100%" }}
+            animate={{ y: "0%" }}
+            exit={{ y: "-100%" }}
+            transition={{ ease: "backIn", duration: 0.75 }}
+            className="text-transparent bg-clip-text bg-gradient-to-r from-orange-600 to-orange-400 text-2xl md:text-4xl lg:text-6xl xl:text-7xl text-black font-medium"
+          >
+            {num}
+          </motion.span>
+        </AnimatePresence>
+      </div>
+    </div>
+  );
+}
